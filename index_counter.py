78522ee6c674e19718912b635621936739ec07ce
Alexander Chirkin <chirkin2001@list.ru>
2023-12-16 20:50:50 +0500

    счетчик взаимного ИС

--- index_counter.py
+++ index_counter.py
@@ -3,20 +3,22 @@ import re
 
 def calculate_index():
     found = {}
-    regexp = re.compile("^[а-яёА-ЯЁ]$")
+    regexp = re.compile("^[а-яё]$")
     for i in range(1, 5):
         with open(f'input/voyna-i-mir-tom-{i}.txt') as tom_file:
-            tom = ''.join(tom_file.readlines()).replace(' ', '').replace('\n', '').replace('\t', '').lower()
+            tom = ''.join(tom_file.readlines()).lower()
         for char in tom:
             if not regexp.match(char):
                 continue
-            if char not in found:
-                found[char] = 1
-            else:
-                found[char] += 1
+            found[char] = 1 if char not in found else found[char] + 1
     total_symbols = sum(found.values())
     freq = {char: found[char] / total_symbols for char in found}
-    print(f'Индекс совпадения: {str(sum([frequency ** 2 for frequency in freq.values()]))}')
+    print(f'Частоты: {sorted(freq.items(), key=lambda x: x[1], reverse=True)}')
+    print(f'Индекс совпадения: {sum([frequency ** 2 for frequency in freq.values()])}')
+    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'  # напишем алфавит так, иначе будут проблемы с "ё"
+    for offset in range(1, 17):  # взаимные ИС для отступов s и 33-s одинаковые
+        print(f'Индекс взаимного совпадения при отступе {offset}: '
+              f'{sum(freq[sym] * freq[alphabet[(alphabet.find(sym) + offset) % 33]] for sym in alphabet)}')
 
 
 if __name__ == '__main__':
